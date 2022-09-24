ce47f7808615bdee79470248553d8439c438f1e3
Prathamesh Ramane <Prathamesh.Ramane@lntinfotech.com>
2022-09-24 18:33:22 +0530

    Database setup

--- backend/settings/development.py
+++ backend/settings/development.py
@@ -14,8 +14,15 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_USER_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
