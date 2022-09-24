29abc0983beaa1ce1d892012204d213516b8a103
Prathamesh Ramane <Prathamesh.Ramane@lntinfotech.com>
2022-09-24 23:26:23 +0530

    Absolute Image URL

--- core/serializers.py
+++ core/serializers.py
@@ -1,4 +1,8 @@
-from rest_framework.serializers import ModelSerializer
+from django.conf import settings
+
+# Rest Framework
+
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 # Models
 from .models import Factory, Product
@@ -11,6 +15,11 @@ class FactorySerializer(ModelSerializer):
 
 
 class ProductSerializer(ModelSerializer):
+    imageUrl = SerializerMethodField()
+
     class Meta:
         model = Product
         fields = '__all__'
+
+    def get_imageUrl(self, instance):
+        return settings.HOST_URL+instance.imageUrl.url
