6aa51e13df4bb040199ed71ef58391ffb9b1c9f4
Prathamesh Ramane <Prathamesh.Ramane@lntinfotech.com>
2022-09-24 19:14:08 +0530

    Database Models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+# Models
+from .models import Factory, Product
+
 # Register your models here.
+admin.site.register(Factory)
+admin.site.register(Product)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
+
+
+class Factory(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "Factories"
+
+    def __str__(self) -> str:
+        return f'{self.name} - {self.location}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    quantity = models.IntegerField(validators=[MinValueValidator(0)])
+    description = models.TextField()
+
+    # Relations
+    factory = models.ForeignKey(to=Factory, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'{self.name} - {self.factory.name}'
