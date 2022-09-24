3894a1fd88addb9f60be1231f6c1a5a62fcbd252
Prathamesh Ramane <Prathamesh.Ramane@lntinfotech.com>
2022-09-24 19:19:29 +0530

    Added Image Field

--- core/models.py
+++ core/models.py
@@ -15,10 +15,15 @@ class Factory(models.Model):
         return f'{self.name} - {self.location}'
 
 
+def product_image_directory_path(instance, filename):
+    return 'products/{0}/{1}'.format(instance.factory.name, filename)
+
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     quantity = models.IntegerField(validators=[MinValueValidator(0)])
     description = models.TextField()
+    imageUrl = models.ImageField(upload_to=product_image_directory_path)
 
     # Relations
     factory = models.ForeignKey(to=Factory, on_delete=models.CASCADE)
