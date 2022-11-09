2fc258024252c13426d047dd6c4970d7bdcc4707
Prathamesh Ramane <Prathamesh.Ramane@lntinfotech.com>
2022-11-09 10:57:08 +0530

    Production Settings

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -1,21 +1,27 @@
-from .base import *
 from decouple import config
+from .base import *
 
 
-APP_URL = ""
-HOST_URL = ""
+APP_URL = "http://localhost:3000"
+HOST_URL = "http://127.0.0.1:8000"
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost']
-
+ALLOWED_HOSTS = ['prathamesh-api.azurewebsites.net']
 
 # Database
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "mssql",
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_USER_PASSWORD'),
+        'HOST': 'get-assessment-server.database.windows.net',
+        "PORT": "1433",
+        "OPTIONS": {
+            "driver": "ODBC Driver 17 for SQL Server",
+        }
     }
 }
 
