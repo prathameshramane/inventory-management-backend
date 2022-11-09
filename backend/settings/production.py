419928bff3502bbda2b760235eabb52b0e5d7f10
Prathamesh Ramane <Prathamesh.Ramane@lntinfotech.com>
2022-11-09 11:17:24 +0530

    Static and CORS

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -2,8 +2,8 @@ from decouple import config
 from .base import *
 
 
-APP_URL = "http://localhost:3000"
-HOST_URL = "http://127.0.0.1:8000"
+APP_URL = "https://prathamesh-inventory-management.azurewebsites.net"
+HOST_URL = "https://prathamesh-api.azurewebsites.net"
 
 DEBUG = False
 
@@ -29,4 +29,6 @@ DATABASES = {
 # CORS
 
 CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'https://prathamesh-inventory-management.azurewebsites.net'
 ]
