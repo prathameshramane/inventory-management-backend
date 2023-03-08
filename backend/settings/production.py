1408f04d50971f9a18fa40b36a8720a8639ee5b9
Prathamesh Ramane <Prathamesh.Ramane@lntinfotech.com>
2023-03-08 12:08:48 +0530

    Added CORS Headers

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -32,3 +32,16 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'https://prathamesh-inventory-management.azurewebsites.net'
 ]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+    "ocp-apim-subscription-key",
+]
